21f37eef925d1e33d78d2e2e528a7e7a435decd7
Stijn Linderman <metstijnonline@gmail.com>
2022-12-07 14:49:39 +0100

    Solved day 7

--- src/puzzle-solvers/day7.js
+++ src/puzzle-solvers/day7.js
@@ -1,5 +1,97 @@
+const LINE_SEPARATOR = "\r\n";
+
 function getSolution (inputDataAsString) {
-    return "solution of day 7";
+    const directoryMap = Directory.createDirectoryMapFromInputData(inputDataAsString);
+    return calculateTotalSizeOfDirectoriesThatAreNotBiggerThan(directoryMap, 100000);
 }
 
-module.exports = {getSolution};
+class Directory {
+    name;
+    parentDirectory;
+    subDirectories = new Map();
+    files = new Map();
+    size;
+
+    static createDirectoryMapFromInputData (inputDataAsString) {
+        const lines = inputDataAsString.split(LINE_SEPARATOR);
+        let rootDirectory = new Directory ("root", null);
+        const rootSubDirectory = new Directory ("/", rootDirectory);
+        rootDirectory.addSubDirectory(rootSubDirectory);
+        let currentDirectory = rootDirectory; // set root directory as starting point for the map
+        lines.forEach((line, index) => { // create the map, starting from the root, by looping through the lines
+            const lineElements = line.split(" ");
+            const firstArgument = lineElements[0];
+            switch (firstArgument) {
+                case "$":
+                    const command = lineElements[1];
+                    switch (command) {
+                        case "cd":
+                            const directoryNameToGoTo = lineElements[2];
+                            switch (directoryNameToGoTo) { // go back to the parent directory
+                                case "..":
+                                    currentDirectory = currentDirectory.parentDirectory;
+                                    break;
+                                default: // go into a subdirectory
+                                    currentDirectory = currentDirectory.subDirectories.get(directoryNameToGoTo);
+                            }
+                            break;
+                        case "ls":
+                            // do nothing
+                            break;
+                        default:
+                            // do nothing
+                    }
+                    break;
+                case "dir": // new subdirectory found, add it
+                    const subDirectoryName = lineElements[1];
+                    const subDirectory = new Directory(subDirectoryName, currentDirectory)
+                    currentDirectory.addSubDirectory(subDirectory);
+                    break;
+                default: // new file found, add it
+                    const fileSize = lineElements[0] * 1; // transform string to number
+                    const fileName = lineElements[1];
+                    currentDirectory.addFile(fileSize, fileName);
+            }
+        })
+        rootDirectory.calculateSizeOfDirectoryAndSubdirectories();
+        return rootDirectory;
+    }
+
+    constructor (directoryName, parentDirectory) {
+        this.name = directoryName;
+        this.parentDirectory = parentDirectory;
+    }
+
+    addSubDirectory (subDirectory) {
+        this.subDirectories.set(subDirectory.name, subDirectory)
+    }
+
+    addFile (fileSize, fileName) {
+        this.files.set(fileName, fileSize);
+    }
+
+    calculateSizeOfDirectoryAndSubdirectories () {
+        this.size = 0;
+        this.subDirectories.forEach((subDirectory) => {
+            subDirectory.calculateSizeOfDirectoryAndSubdirectories();
+            this.size += subDirectory.size;
+        })
+        this.files.forEach((fileSize) => {
+            this.size += fileSize;
+        })
+    }
+}
+
+function calculateTotalSizeOfDirectoriesThatAreNotBiggerThan(directory, maximumSize) {
+    let totalSize = 0;
+    directory.subDirectories.forEach((subDirectory) => {
+        const subDirectorySize = subDirectory.size;
+        if (subDirectorySize && subDirectorySize <= maximumSize) {
+            totalSize += subDirectorySize;
+        }
+        totalSize += calculateTotalSizeOfDirectoriesThatAreNotBiggerThan(subDirectory, maximumSize);
+    })
+    return totalSize;
+}
+
+module.exports = {getSolution, Directory};
--- test/puzzle-solvers/day7.test.js
+++ test/puzzle-solvers/day7.test.js
@@ -4,9 +4,9 @@ const { expect } = require("chai");
 describe ('day 7 getSolution', () => {
     let input;
 
-/*     it ('test case', () => {
-        input = "?";
-        expect(getSolution(input)).to.equal(11);
-    }) */
+    it ('test case', () => {
+        input = "$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k";
+        expect(getSolution(input)).to.equal(95437);
+    })
 
 })
